49e8753ce13416e8a6e56bc5a8c278caff74fb7a
cku <xidanhgiaychuan@gmail.com>
2026-01-25 12:07:55 +0700

    done caculator

--- caculator-mini/index.js
+++ caculator-mini/index.js
@@ -1,13 +1,75 @@
+let buttonContent = "C/*-789+456=1230.";
 const buttonContainer = document.querySelector(".button-container");
+let inputField = document.getElementById("result");
+let currentValue = "";
 
-let buttonContent = "1234567890.C/*-+";
+createButton();
+
+const buttonBtn = document.querySelectorAll("button");
+
+buttonBtn.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    const buttonValue = btn.textContent;
+
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      caculate();
+    } else {
+      //only use string
+      appendValue(buttonValue);
+    }
+  }),
+);
+
+// Function
+
+function caculate() {
+  inputField.value = eval(inputField.value);
+}
+
+function clearResult() {
+  inputField.value = "";
+}
+
+function appendValue(buttonValue) {
+  // check toan tu
+  if (!isNaN(buttonValue) || buttonValue == ".") {
+    currentValue += buttonValue;
+    inputField.value = currentValue;
+  } else if (["+", "-", "*", "/"].includes(buttonValue)) {
+    const lastChar = currentValue.slice(-1);
+
+    if (["+", "-", "*", "/"].includes(lastChar)) {
+      currentValue = currentValue.slice(0, -1) + buttonValue;
+    } else {
+      currentValue += buttonValue;
+    }
+  } else {
+    currentValue += buttonValue;
+    console.log({ currentValue });
+  }
+
+  inputField.value = currentValue;
+}
 
 function createButton() {
   for (let i = 0; i < buttonContent.length; i++) {
-    const button = document.createElement("button");
-    button.innerText = buttonContent[i];
+    const content = buttonContent[i];
+    let button = document.createElement("button");
+    button.innerText = content;
+
+    if (!isNaN(content)) {
+      button.classList.add("btn-number");
+    } else if (["+", "-", "*", "/"].includes(content)) {
+      button.classList.add("btn-operator");
+    } else if (content === "C") {
+      button.classList.add("btn-clear");
+    } else if (content === "=") {
+      button.classList.add("btn-equals");
+    } else {
+      button.classList.add("btn-decimal");
+    }
     buttonContainer.appendChild(button);
   }
 }
-
-createButton();
