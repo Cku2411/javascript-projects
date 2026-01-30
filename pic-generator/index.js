7a6af47d92e112637e71913f36165bbc718b49c7
cku <xidanhgiaychuan@gmail.com>
2026-01-30 07:49:05 +0700

    7:34

--- pic-generator/index.js
+++ pic-generator/index.js
@@ -1,40 +1,7 @@
-const inputElm = document.querySelector(".input");
-const form = document.querySelector(".form");
-const lists = document.querySelector(".list");
+const btnElm = document.querySelector(".btn");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+const imgHolder = document.querySelector(".img-holder");
 
-  const task = inputElm.value.trim();
-
-  // create li
-  const liElm = document.createElement("li");
-  liElm.textContent = task;
-
-  const optionElm = document.createElement("div");
-  optionElm.classList.add("option");
-
-  const checkBox = document.createElement("i");
-  checkBox.classList.add("fa-solid", "fa-square-check");
-
-  checkBox.addEventListener("click", () => {
-    liElm.classList.toggle("checked");
-  });
-
-  const trashIcon = document.createElement("i");
-  trashIcon.classList.add("fa-solid", "fa-trash");
-
-  // Thêm sự kiện cho icon trash
-  trashIcon.addEventListener("click", () => {
-    liElm.remove();
-  });
-
-  optionElm.appendChild(checkBox);
-  optionElm.appendChild(trashIcon);
-  liElm.appendChild(optionElm);
-
-  lists.appendChild(liElm);
-
-  // Reset input
-  inputElm.value = "";
+btnElm.addEventListener("click", () => {
+  imgHolder.classList.toggle("show");
 });
